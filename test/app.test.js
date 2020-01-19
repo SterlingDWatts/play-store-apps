7198c277fb1577936c335ad1054f28ef5374ec1b
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-01-19 12:19:47 -0800

    Start test suite for GET /apps endpoint

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const supertest = require('supertest');
+const { expect } = require('chai');
+const app = require('../app');
+
+/*
+describe(`GET /apps endpoint`, () => {
+
+    it('should')
+
+})
+*/
