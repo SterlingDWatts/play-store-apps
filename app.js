4da9de9c077d464242b71405d4772539c86e515b
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-01-19 12:12:52 -0800

    Move server to server.js for testing

--- app.js
+++ app.js
@@ -57,6 +57,4 @@ app.get('/apps', (req, res) => {
 
 });
 
-app.listen(8000, () => {
-    console.log('Yo, I started that server for you on PORT 8000!!')
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+app.listen(8000, () => {
+    console.log('Yo, I started that server for you on PORT 8000!!')
+});
