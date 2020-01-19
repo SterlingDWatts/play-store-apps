cbbeb4786dde16b84e76b7575e5f151fbf841722
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-01-19 12:32:49 -0800

    Create first test for test suite

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,21 @@ const supertest = require('supertest');
 const { expect } = require('chai');
 const app = require('../app');
 
-/*
 describe(`GET /apps endpoint`, () => {
 
-    it('should')
+    it('should return an array of apps', () => {
+        return supertest(app)
+            .get('/apps')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body).to.be.an('array');
+                expect(res.body).to.have.lengthOf.at.least(1);
+                const app = res.body[0];
+                expect(app).to.include.all.keys(
+                    'App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type', 'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver', 'Android Ver'
+                );
+            });
+    });
 
-})
-*/
+});
